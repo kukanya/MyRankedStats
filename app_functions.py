211f382a3fbe993011ab7cee61894bc5ec86e8b3
kukanya <kukanya.nya@gmail.com>
2016-01-08 14:33:49 +0300

    :tangerine: Stats redo

--- app_functions.py
+++ app_functions.py
@@ -58,19 +58,61 @@ def process_matches_data(api: PersonalAPI, db: DB, target: dict):
     db.insert("matches", matches)
 
 
-def get_stats(db: DB, params={}):
-    matches = db.get_data("matches", params)
-    if len(params):
-        print(reduce(lambda x, y: "{}, {}".format(x, y), map(lambda p: "{} = {}".format(p, params[p]), params)))
-    print(len(matches), "games played: ", end="")
-    wins = reduce(lambda x, y: x+y, map(lambda m: m["winner"], matches))
-    print(wins, "wins,", len(matches)-wins, "losses")
-    print("{0:.0f}% win rate".format(wins/len(matches)*100))
-    kills = reduce(lambda x, y: x+y, map(lambda m: m["kills"], matches))
-    deaths = reduce(lambda x, y: x+y, map(lambda m: m["deaths"], matches))
-    assists = reduce(lambda x, y: x+y, map(lambda m: m["assists"], matches))
-    print("KDA: {0:.2f}".format((kills+assists)/deaths))
+def sum_stat(stat, matches):
+    return reduce(lambda x, y: x+y, map(lambda m: m[stat], matches))
 
-# if __name__ == '__main__':
-#     db = DB()
-#     get_stats(db, {"championId": [267]})
+
+def calc_kda(k, d, a):
+    if d:
+        return (k+a)/d
+    else:
+        return k+a
+
+
+def calc_basic_stats(matches):
+    stats = dict()
+    stats["games"] = len(matches)
+    stats["wins"] = sum_stat("winner", matches)
+    stats["losses"] = stats["games"] - stats["wins"]
+    stats["win_rate"] = stats["wins"]/stats["games"]*100
+    stats["kills"] = sum_stat("kills", matches)
+    stats["deaths"] = sum_stat("deaths", matches)
+    stats["assists"] = sum_stat("assists", matches)
+    stats["kda"] = calc_kda(stats["kills"], stats["deaths"], stats["assists"])
+    stats["av_kills"] = stats["kills"]/stats["games"]
+    stats["av_deaths"] = stats["deaths"]/stats["games"]
+    stats["av_assists"] = stats["assists"]/stats["games"]
+    return stats
+
+
+def print_basic_stats(stats):
+    print(stats["games"], "games played: ", end="")
+    print(stats["wins"], "wins,", stats["losses"], "losses")
+    print("{0:.0f}% win rate".format(stats["win_rate"]))
+    print("KDA: {0:.2f}".format(stats["kda"]))
+    print("Average K/D/A: {0:.1f}/".format(stats["av_kills"]) +
+          "{0:.1f}/".format(stats["av_deaths"]) + "{0:.1f}".format(stats["av_assists"]))
+    print("Total K/D/A: {}/{}/{}".format(stats["kills"], stats["deaths"], stats["assists"]))
+
+
+def calc_champions_stats(matches):
+    champ_stats = []
+    for champ_id in set(map(lambda m: m["championId"], matches)):
+        champ_stats.append({
+            "championId": champ_id,
+            "stats": calc_basic_stats(list(filter(lambda m: m["championId"] == champ_id, matches)))
+        })
+    return champ_stats
+
+
+def get_role_stats(db: DB, roles: list):
+    matches = db.get_data("matches", {"role": roles})
+    stats = calc_basic_stats(matches)
+    stats["champions_stats"] = calc_champions_stats(matches)
+    print("ROLE(S):", reduce(lambda x, y: x+", "+y, roles))
+    print("ROLE(S) STATS:")
+    print_basic_stats(stats)
+    print("BY CHAMPION:")
+    for champ in stats["champions_stats"]:
+        print("championId:", champ["championId"])
+        print_basic_stats(champ["stats"])
